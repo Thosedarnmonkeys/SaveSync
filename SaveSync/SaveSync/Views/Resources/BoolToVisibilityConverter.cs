d7fecab28194da5a46fca385edad3066fac3bf7a
Thosedarnmonkeys <thosedarnmonkeys@googlemail.com>
2018-03-01 12:02:08 +0000

    Top ui starting not to look like madness

---
+++ SaveSync/SaveSync/Views/Resources/BoolToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SaveSync.Views.Resources
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+      public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+      {
+        bool current = (bool) value;
+        bool target = (bool) parameter;
+
+        return current == target ? Visibility.Visible : Visibility.Collapsed;
+      }
+
+      public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+      {
+        throw new NotImplementedException();
+      }
+    }
+}
