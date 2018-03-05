e2c949528fda552ad9490157094af4868f018b8f
Thosedarnmonkeys <thosedarnmonkeys@googlemail.com>
2018-03-05 17:56:40 +0000

    Started adding edit ui for mappings

--- SaveSync/SaveSync/ViewModels/MappingViewModel.cs
+++ SaveSync/SaveSync/ViewModels/MappingViewModel.cs
@@ -3,14 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using SaveSync.Mapping;
 
 namespace SaveSync.ViewModels
 {
-  public class MappingViewModel
+  public class MappingViewModel : DependencyObject
   {
     public FolderMapping Mapping { get; set; }
-    public DateTime ServerAge { get; set; }
-    public DateTime ClientAge { get; set; }
+
+    #region ServerAge
+    private DateTime serverAge;
+    public DateTime ServerAge
+    {
+      get { return serverAge; }
+      set
+      {
+        serverAge = value;
+        LocalNewer = ClientAge > ServerAge;
+      }
+    }
+    #endregion
+
+    #region ClientAge
+    private DateTime clientAge;
+    public DateTime ClientAge
+    {
+      get { return clientAge; }
+      set
+      {
+        clientAge = value;
+        LocalNewer = ClientAge > ServerAge;
+      }
+    }
+    #endregion
+
+    #region LocalNewer
+    public bool LocalNewer
+    {
+      get { return (bool)GetValue(LocalNewerProperty); }
+      set { SetValue(LocalNewerProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for LocalNewer.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty LocalNewerProperty =
+        DependencyProperty.Register("LocalNewer", typeof(bool), typeof(MainViewModel), new PropertyMetadata(false)); 
+    #endregion
+
+    public string ServerAgeString
+    {
+      get
+      {
+        if (ServerAge == DateTime.MinValue)
+          return "-";
+
+        return ServerAge.ToString();
+      }
+    }
+
+    public string ClientAgeString
+    {
+      get
+      {
+        if (ClientAge == DateTime.MinValue)
+          return "-";
+
+        return ClientAge.ToString();
+      }
+    }
   }
 }
---
+++ SaveSync/SaveSync/Views/EditMappingWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SaveSync.ViewModels;
+
+namespace SaveSync.Views
+{
+  /// <summary>
+  /// Interaction logic for EditMappingWindow.xaml
+  /// </summary>
+  public partial class EditMappingWindow : Window
+  {
+    public EditMappingWindow(MappingViewModel vm)
+    {
+      InitializeComponent();
+      DataContext = vm;
+    }
+  }
+}
